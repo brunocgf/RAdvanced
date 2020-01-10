441980ae961bf976b1dce46b764fc3c1e4e8aed5
Bruno Gonzalez <brunocgf@gmail.com>
2020-01-10 17:55:05 -0600

    s3

--- Vectors.R
+++ Vectors.R
@@ -22,3 +22,39 @@ attr(a, "x") <- "abcdef"
 attr(a, "x")
 
 structure(1:5, comment = "my attribute")
+
+
+# S3 atomic vectors -------------------------------------------------------
+
+x <- factor(c("a", "b", "b", "a"))
+
+typeof(x)
+
+attributes(x)
+
+## What sort of object does table() return? What is its type? What attributes does it have?
+## How does the dimensionality change as you tabulate more variables?
+
+t <- table(c(1,2,3,2,1))
+typeof(t)
+attributes(t)
+
+## What happens to a factor when you modify its levels?
+
+f1 <- factor(letters)
+levels(f1) <- rev(levels(f1))
+
+## What does this code do? How do f2 and f3 differ from f1?
+
+f2 <- rev(factor(letters))
+
+f3 <- factor(letters, levels = rev(letters))
+
+
+# Lists -------------------------------------------------------------------
+
+## Why do you need to use unlist() to convert a list to an atomic vector? Why doesn’t as.vector() work?
+
+l <- list(1,2,c(3,4))
+
+
